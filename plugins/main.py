0d5f36fd7119db784649dd9eceaaae3c4bd1c53a
ARNAV SINGH <116391676+Xdarnav@users.noreply.github.com>
2022-11-06 16:47:44 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -1,31 +1,31 @@
 import os
 from database import database
 from plugins.helpers import Helpers
-from pyArnavje import Arnavje, Message, filters
+from pystark import Stark, Message, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 
-@Arnavje.cmd(private_only=True, extra_filters=filters.video | filters.animation)
+@Stark.cmd(private_only=True, extra_filters=filters.video | filters.animation)
 async def main(_, msg: Message):
     status = await msg.reply("Doing what I do best...")
-    Arnavje = Helpers(msg, status)
-    await process(msg, Arnavje, status)
-    if os.path.exists(Arnavje.input_file):
-        os.remove(Arnavje.input_file)
-    if os.path.exists(Arnavje.output_file):
-        os.remove(Arnavje.output_file)
+    stark = Helpers(msg, status)
+    await process(msg, stark, status)
+    if os.path.exists(stark.input_file):
+        os.remove(stark.input_file)
+    if os.path.exists(stark.output_file):
+        os.remove(stark.output_file)
 
 
-async def process(msg: Message, Arnavje: Helpers, status: Message):
-    await msg.download(Arnavje.input_file)
-    std = await Arnavje.subshell()
-    if os.path.exists(Arnavje.output_file):
-        await Arnavje.correct_the_size()  # File is too big error
+async def process(msg: Message, stark: Helpers, status: Message):
+    await msg.download(stark.input_file)
+    std = await stark.subshell()
+    if os.path.exists(stark.output_file):
+        await stark.correct_the_size()  # File is too big error
         await status.edit('Getting your pack...')
-        success, pack_name, title = await Arnavje.get_default_pack()
-        should_ask = await database.get('users', Arnavje.user_id, 'ask_emojis')
+        success, pack_name, title = await stark.get_default_pack()
+        should_ask = await database.get('users', stark.user_id, 'ask_emojis')
         if should_ask:
-            emojis = await Arnavje.ask_for_emojis()
+            emojis = await stark.ask_for_emojis()
             if not emojis:
                 return
         else:
@@ -34,17 +34,17 @@ async def process(msg: Message, Arnavje: Helpers, status: Message):
                 emojis = data
             else:
                 emojis = '💖'
-        params = await Arnavje.params(pack_name, emojis, title)
-        file = open(Arnavje.output_file, 'rb')
+        params = await stark.params(pack_name, emojis, title)
+        file = open(stark.output_file, 'rb')
         if success:
-            success = await Arnavje.add_to_pack(params, file)
+            success = await stark.add_to_pack(params, file)
             if not success:
                 return
         else:
-            success = await Arnavje.new_pack(params, file)
+            success = await stark.new_pack(params, file)
             if not success:
                 return
-        sticker = await Arnavje.get_pack(params, file)
+        sticker = await stark.get_pack(params, file)
         await status.delete()
         if sticker:
             button = [[InlineKeyboardButton('Get WEBM File', callback_data='current_webm')]]
@@ -54,31 +54,31 @@ async def process(msg: Message, Arnavje: Helpers, status: Message):
                 reply_markup=InlineKeyboardMarkup(button)
             )
         file.close()
-        await Arnavje.session.aclose()
+        await stark.session.aclose()
     else:
-        await Arnavje.ffmpeg_error(std)
+        await stark.ffmpeg_error(std)
         return
 
 
-@Arnavje.callback('current_webm')
+@Stark.callback('current_webm')
 async def get_webm(_, callback: CallbackQuery):
     await Helpers.send_webm(callback.message)
     await callback.answer()
 
 
-@Arnavje.cmd(private_only=True, extra_filters=filters.sticker, group=1)
+@Stark.cmd(private_only=True, extra_filters=filters.sticker, group=1)
 async def existing_sticker_func(_, msg: Message):
     if msg.sticker.is_video:
         data = await database.get('users', msg.from_user.id)
         if data['kang_mode']:
             status = await msg.reply("Doing what I do best [Kanging]... ")
             sticker_file = await msg.download(f'kangs/{msg.from_user.id}_{msg.message_id}.webm')
-            Arnavje = Helpers(msg, status)
+            stark = Helpers(msg, status)
             await status.edit('Getting your pack...')
-            success, pack_name, title = await Arnavje.get_default_pack()
-            should_ask = await database.get('users', Arnavje.user_id, 'ask_emojis')
+            success, pack_name, title = await stark.get_default_pack()
+            should_ask = await database.get('users', stark.user_id, 'ask_emojis')
             if should_ask:
-                emojis = await Arnavje.ask_for_emojis()
+                emojis = await stark.ask_for_emojis()
                 if not emojis:
                     return
             else:
@@ -87,17 +87,17 @@ async def existing_sticker_func(_, msg: Message):
                     emojis = data
                 else:
                     emojis = '💖'
-            params = await Arnavje.params(pack_name, emojis, title)
+            params = await stark.params(pack_name, emojis, title)
             file = open(sticker_file, 'rb')
             if success:
-                success = await Arnavje.add_to_pack(params, file)
+                success = await stark.add_to_pack(params, file)
                 if not success:
                     return
             else:
-                success = await Arnavje.new_pack(params, file)
+                success = await stark.new_pack(params, file)
                 if not success:
                     return
-            sticker = await Arnavje.get_pack(params, file)
+            sticker = await stark.get_pack(params, file)
             await status.delete()
             if sticker:
                 await msg.reply_sticker(
@@ -106,6 +106,6 @@ async def existing_sticker_func(_, msg: Message):
                 )
             file.close()
             os.remove(sticker_file)
-            await Arnavje.session.aclose()
+            await stark.session.aclose()
         elif data['get_webm']:
             await Helpers.send_webm(msg)
