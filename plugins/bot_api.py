4fb76fd4b307d4a9da04d27e4c036392835406aa
ARNAV SINGH <116391676+Xdarnav@users.noreply.github.com>
2022-11-06 17:29:49 +0530

    Update bot_api.py

--- plugins/bot_api.py
+++ plugins/bot_api.py
@@ -12,7 +12,7 @@ class BotAPI:
     new_pack_url = f"{base_url}createNewStickerSet"
     get_pack_url = f"{base_url}getStickerSet"
     add_to_pack_url = f"{base_url}addStickerToSet"
-    ERROR = "Oops, an error occurred. My owner has been notified. \n\nFor queries visit @link_copied"
+    ERROR = "Oops, an error occurred. My owner has been notified. \n\nFor queries visit @StarkBotsChat"
     username = requests.get(base_url + "getMe").json()["result"]["username"].title()
     PACK_NAME = "fpack_{}_by_" + username
     NEW_PACK_NAME = "fpack{}_{}_by_" + username
@@ -65,7 +65,7 @@ class BotAPI:
             await self.error(resp, params['name'])
         except TooManyRequests as e:
             msg = self.message
-            err = f"Error from Telegram \n\n{e.desc} \n\nFor queries visit @link_copied"
+            err = f"Error from Telegram \n\n{e.desc} \n\nFor queries visit @StarkBotsChat"
             await msg.reply(err, quote=True)
             await self.client.send_message(
                 self.LOG_CHAT,
@@ -105,7 +105,7 @@ class BotAPI:
                     return False
         return True
 
-    @cutearnav143
+    @staticmethod
     async def error(resp, pack_name):
         if not resp['ok']:
             desc = resp['description']
